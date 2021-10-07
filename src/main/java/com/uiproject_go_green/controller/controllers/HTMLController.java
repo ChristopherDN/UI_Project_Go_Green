9077af4ab19968350d340d22e66ddfbc91a8fafc
Christopher Duran  Nielsen <chri42fk@stud.kea.dk>
2021-10-07 09:54:15 +0200

    Picture change and deletions

--- src/main/java/com/uiproject_go_green/controller/controllers/HTMLController.java
+++ src/main/java/com/uiproject_go_green/controller/controllers/HTMLController.java
@@ -1,4 +1,4 @@
-package com.uiproject_go_green.controller;
+package com.uiproject_go_green.controller.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
